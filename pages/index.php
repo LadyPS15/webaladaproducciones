2e01c93ad92b08cebec0a255fe3d58bdaea76d78
Lady Perez <ladydiperezsimon@gmail.com>
2025-06-10 12:42:47 -0500

    Update index.php

--- pages/index.php
+++ pages/index.php
@@ -15,7 +15,7 @@ if (!empty($_SESSION['user'])) {
     <link rel="icon" href="../img/logo_alada.png" type="image/png">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600&family=Playfair+Display:wght@400;500;600&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
     <style>
         /* Estilos para el control de audio */
         .audio-control {
